0e97bba115839783ac3fe5de2e0242dec9ea570e
Felipe Cortez <hola@faco.cl>
2021-01-14 14:06:35 -0300

    Update script.js

--- clase-05/script.js
+++ clase-05/script.js
@@ -13,51 +13,57 @@ Papa.parse("https://raw.githubusercontent.com/profesorfaco/dno037-2021/main/clas
         var datos = respuesta.data;
         // en caso resulte una última fila con "basurita", usar datos.pop();
         var url = window.location.pathname;
-        if (url.includes("index") || url.length === 0 || url === '/') {
+        console.log(url);
+        if (url.includes("index") || url.length == 0 || url == "/") {
             datos.forEach(function (dato, i) {
                 document.getElementsByTagName("tbody")[0].innerHTML += "<tr><td><a href='page.html?id=" + i + "'>" + dato.pais + "</td><td>" + dato.diez + "</td><td>" + dato.once + "</td><td>" + dato.doce + "</td><td>" + dato.trece + "</td><td>" + dato.catorce + "</td><td>" + dato.quince + "</td><td>" + dato.dieciseis + "</td></tr>";
             });
         } else {
             var x = new URLSearchParams(window.location.search).get("id");
-            document.getElementsByTagName("h1")[0].append(datos[x].pais);
-            // crear un arreglo y empujarle elementos
-            var indice = [];
-            indice.push(datos[x].diez);
-            indice.push(datos[x].once);
-            indice.push(datos[x].doce);
-            indice.push(datos[x].trece);
-            indice.push(datos[x].catorce);
-            indice.push(datos[x].quince);
-            indice.push(datos[x].dieciseis);
-            // un inception de bibliotecas de JavaScript
-            new Chart(document.getElementById("grafico").getContext("2d"), {
-                type: "line",
-                data: {
-                    labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016],
-                    datasets: [
-                        {
-                            label: "Índice de intesidad de uso del cobre",
-                            data: indice,
-                            backgroundColor: "rgba(184, 115, 51, 0.5)",
-                            borderColor: "rgba(184, 115, 51, 1)",
-                            pointRadius: 2,
-                            borderWidth: 1,
-                            lineTension: 0,
-                        }
-                    ]
-                },
-                options: {
-                    scales: {
-                        yAxes: [
+            console.log(x);
+            if (x !== null) {
+                document.getElementsByTagName("h1")[0].append(datos[x].pais);
+                // crear un arreglo y empujarle elementos
+                var indice = [];
+                indice.push(datos[x].diez);
+                indice.push(datos[x].once);
+                indice.push(datos[x].doce);
+                indice.push(datos[x].trece);
+                indice.push(datos[x].catorce);
+                indice.push(datos[x].quince);
+                indice.push(datos[x].dieciseis);
+                // un inception de bibliotecas de JavaScript
+                new Chart(document.getElementById("grafico").getContext("2d"), {
+                    type: "line",
+                    data: {
+                        labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016],
+                        datasets: [
                             {
-                                ticks: {
-                                    beginAtZero: true,
-                                }
-                            }
-                        ]
-                    }
-                }
-            }); // cierra new Chart
+                                label: "Índice de intesidad de uso del cobre",
+                                data: indice,
+                                backgroundColor: "rgba(184, 115, 51, 0.5)",
+                                borderColor: "rgba(184, 115, 51, 1)",
+                                pointRadius: 2,
+                                borderWidth: 1,
+                                lineTension: 0,
+                            },
+                        ],
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [
+                                {
+                                    ticks: {
+                                        beginAtZero: true,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                }); // cierra new Chart
+            } else {
+                document.getElementsByTagName("h1")[0].innerHTML = "Algo salió mal &#128557";
+            } // cierra else
         } // cierra else
-    } // cierra function(respuesta){} 
+    }, // cierra function(respuesta){}
 }); // cierra Papa.parse
