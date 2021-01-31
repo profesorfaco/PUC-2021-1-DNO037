bfd768c574bdfa83c38b0242fa723477fd71b363
Felipe Cortez <hola@faco.cl>
2021-01-31 00:20:00 -0300

    Add files via upload

---
+++ clase-10/script.js
@@ -0,0 +1,15 @@
+function primeraRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#D34145";
+}
+function segundaRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#5790A3";
+}
+function terceraRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#6A5D9C";
+}
+function cuartaRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#117C60";
+}
+function nada() {
+    document.getElementsByTagName("body")[0].style.background = "#222";
+}
