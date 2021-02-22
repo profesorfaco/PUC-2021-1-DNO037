56fe3b8ce254dab1b8f0de2bfb89270f0a5a4b34
Felipe Cortez <hola@faco.cl>
2021-02-22 11:45:51 -0300

    Add files via upload

--- clase-06/script.js
+++ clase-06/script.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
         console.log(data);
         if (document.body.classList.contains("portada")) {
             data.features.forEach(function (temblor, i) {
-                let division;
+                let articuloTipo;
                 if (temblor.properties.place.includes("Chile")) {
-                    division = '<div class="chileno">';
+                    articuloTipo = '<article class="chileno">';
                 } else {
-                    division = "<div>";
+                    articuloTipo = "<article>";
                 }
-                $("section").append(division + "<h2>" + temblor.properties.mag + "</h2><p>" + temblor.properties.place + "</p><p><a href='page.html?temblor=" + i + "'>ver mapa</a></p></div>");
+                $("section").append(articuloTipo + "<h2>" + temblor.properties.mag + "</h2><p>" + temblor.properties.place + "</p><p><a href='page.html?temblor=" + i + "'>ver mapa</a></p></article>");
             });
         } else {
             let t = new URLSearchParams(window.location.search).get("temblor");
@@ -18,23 +18,27 @@ $(document).ready(function () {
                 let longitud = data.features[t].geometry.coordinates[0];
                 let latitud = data.features[t].geometry.coordinates[1];
                 let more = data.features[t].properties.url;
-                $("h1").append(nombre);
                 let elmapa = L.map("mapa").setView([latitud, longitud], 3);
                 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
                     maxZoom: 18,
-                    id: "mapbox/light-v10",
+                    id: "mapbox/dark-v10",
                     tileSize: 512,
                     zoomOffset: -1,
                 }).addTo(elmapa);
                 circle = L.circle([latitud, longitud], {
-                    fillColor: "#FF4500",
+                    fillColor: "cyan",
                     fillOpacity: 0.8,
                     weight: 0,
                     radius: 100000,
-                }).addTo(elmapa);
-                $("footer p").append('<a href="' + more + '" style="float:right" target="_blank" rel="noopener noreferrer">Más detalles</a>');
+                })
+                    .addTo(elmapa)
+                    .on("click", vinculo);
+                function vinculo() {
+                    window.open(more, "_blank", "noopener");
+                }
+                $("header h2").append(" — " + nombre);
             } else {
-                $("h1").append("Algo salió mal &#128557;");
+                $("h2").append(" — Algo salió mal &#128557;");
             } // cierro un else
         } // cierro un else
     }); // cierro $.getJSON({})
