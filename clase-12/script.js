334126820089f2170d88534675d39c6bd7037b62
Felipe Cortez <hola@faco.cl>
2021-06-02 00:43:32 -0400

    Add files via upload

--- clase-12/script.js
+++ clase-12/script.js
@@ -1,3 +1,4 @@
+//El scroll
 var secundario = document.getElementById("secundario").offsetHeight;
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
@@ -9,29 +10,34 @@ window.addEventListener("scroll", (event) => {
         document.getElementById("principal").classList.remove("fixed-top");
     }
 });
-var portfolio = [
-    { title: "Lorem ipsum dolor sit amet", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=1" },
-    { title: "Ut sit amet magna lacinia", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=2" },
-    { title: "Aenean ultrices eros nec dui porttitor", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=3" },
-    { title: "Mauris fermentum eros vitae ante efficitur", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=4" },
-    { title: "Integer porttitor neque id pulvinar facilisis", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=5" },
-    { title: "Mauris ut ante rutrum", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=6" },
-    { title: "Sed eu ligula ac odio bibendum faucibus eu et tellus", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=7" },
-    { title: "Aliquam imperdiet eros ultricies risus dignissim", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=8" },
-    { title: "Cras id tincidunt nisl", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=9" },
-    { title: "Mauris ut ante rutrum", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=10" },
-    { title: "Sed eu ligula ac odio bibendum faucibus eu et tellus", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=11" },
-    { title: "Aenean ultrices eros nec dui porttitor", text: "Vestibulum sit amet augue congue sem rutrum dictum.", image: "https://picsum.photos/300?random=12" },
-];
-portfolio.forEach(function (trabajo) {
-    document.getElementById("portfolio").innerHTML +=
-        '<div class="col-sm-6 col-lg-4 col-xxl-3"><div class="card shadow-sm"><img src="' +
-        trabajo.image +
-        '" class="card-img-top" alt="' +
-        trabajo.title +
-        '"><div class="card-body"><h5 class="card-title">' +
-        trabajo.title +
-        '</h5><p class="card-text">' +
-        trabajo.text +
-        "</p></div></div></div>";
+//La API
+fetch("https://sheet.best/api/sheets/41b6b909-b785-450e-9d8d-3bc622324fb8")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+    var portfolio = data;
+    portfolio.forEach(function (trabajo) {
+        if (trabajo.tipo.includes("graphic")) {
+        especialidad = ' data-especialidad="grafica" ';
+        } else {
+        especialidad = ' data-especialidad="otra" ';
+        }
+        document.getElementById("portfolio").innerHTML +=
+            '<div class="col-sm-6 col-lg-4 col-xxl-3"'+
+            especialidad
+            +'><div class="card shadow-sm"><img src="' +
+            trabajo.image +
+            '" class="card-img-top" alt="' +
+            trabajo.title +
+            '"><div class="card-body"><h5 class="card-title">' +
+            trabajo.title +
+            '</h5><p class="card-text">' +
+            trabajo.text +
+            "</p></div></div></div>";
+        });
+    })
+.catch((err) => {
+    console.log("Pucha, algo falló", err);
 });
